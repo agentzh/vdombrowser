262ae683684a3d87bbe16693824335b0388f68c9
agentzh <agentzh@82b2e94c-9313-0410-8ced-81db5252d7f6>
2009-08-19 03:21:32 +0000

    updated build.sh
    git-svn-id: http://svn1.bj.corp.yahoo.com/repos/WebEng/lifesearch/vdom-webkit/VdomBrowser@62192 82b2e94c-9313-0410-8ced-81db5252d7f6

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 export QT_WEBKIT=webkit_trunk
 export WEBKITDIR=$HOME/webkit/WebKit
 if [ -z "$QTDIR" ]; then
-    export QTDIR=/opt/qt4.5
+    export QTDIR=/export/vdom/qt
 fi
 export PATH=$QTDIR/bin:$PATH
     #make distclean
-/opt/qt4.5/bin/qmake "CONFIG-=debug" -r
+/export/vdom/qt/bin/qmake "CONFIG-=debug" -r
 #qmake -r
 make -j2
 
