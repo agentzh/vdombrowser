e2d1316d015db9cd8b37343dfc930fa2de214dc9
agentzh <agentzh@82b2e94c-9313-0410-8ced-81db5252d7f6>
2009-08-19 03:21:27 +0000

    removed useless stuffs
    git-svn-id: http://svn1.bj.corp.yahoo.com/repos/WebEng/lifesearch/vdom-webkit/VdomBrowser@62191 82b2e94c-9313-0410-8ced-81db5252d7f6

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if [ -z "$QTDIR" ]; then
     export QTDIR=/opt/qt4.5
 fi
 export PATH=$QTDIR/bin:$PATH
-#make distclean
+    #make distclean
 /opt/qt4.5/bin/qmake "CONFIG-=debug" -r
 #qmake -r
 make -j2
